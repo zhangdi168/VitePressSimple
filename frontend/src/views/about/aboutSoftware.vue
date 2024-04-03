1aa498a34d9f8e12834c319b3ce47c46475aa08e
ddd <1778871523@qq.com>
2024-04-03 18:07:00 +0800

    完善声明

--- frontend/src/views/about/aboutSoftware.vue
+++ frontend/src/views/about/aboutSoftware.vue
@@ -1,34 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { OpenURL } from "../../../wailsjs/go/system/SystemService";
+</script>
 
 <template>
   <div>
     <div class="mx-20 mt-4 flex justify-center border p-3">
       <span class="text-3xl font-bold text-center">关于软件</span>
-      <div>
+      <div class="px-4">
         <p>
           本软件以Vue3、TypeScript与Wails技术栈为核心，旨在为VitePress用户提供前所未有的便捷与可视化配置体验。
         </p>
 
-        <h2>1. 软件名称与开源性质</h2>
-
         <ul>
           <li>
-            本软件命名为“<strong>[软件名]</strong>”，遵循开放源代码的原则，我们已将其源代码在<a
-              href="[指定开源平台（如GitHub、GitLab等）链接]"
-              ><strong>[指定开源平台]</strong></a
-            >上完全公开，遵循<strong
-              >[具体开源许可协议（如MIT、Apache 2.0等）]</strong
-            >。
+            本软件命名为“<strong>VitePressSimple</strong>”(简称VPSimple)，遵循开放源代码的原则，我们已将其源代码在
+            <span
+              class="cursor-pointer"
+              @click="OpenURL('https://gitee.com/zhangdi168/VitePressSimple')"
+              ><strong>github</strong></span
+            >上完全公开，遵循<strong>Apache 2.0</strong>。
           </li>
           <li>
             任何个人或组织皆可自由访问、下载、使用、研究、修改及再分发本软件，只需遵守相应开源许可协议的规定。
           </li>
         </ul>
 
-        <h2>2. 主要功能与价值</h2>
-
         <p>
-          “<strong>[软件名]</strong>”专注于提升VitePress项目的配置效率与用户体验，其核心特性如下：
+          “<strong>VitePressSimple</strong>”专注于提升VitePress项目的配置效率与用户体验，其核心特性如下：
         </p>
 
         <ul>
@@ -50,10 +48,8 @@
           </li>
         </ul>
 
-        <h2>3. 用户支持与社区参与</h2>
-
         <p>
-          我们诚挚欢迎广大用户试用“<strong>[软件名]</strong>”，并期待您的宝贵反馈与建议。您可以通过以下途径与我们互动：
+          我们诚挚欢迎广大用户试用“<strong>VitePressSimple</strong>”，并期待您的宝贵反馈与建议。您可以通过以下途径与我们互动：
         </p>
 
         <ul>
@@ -69,19 +65,13 @@
           <li>
             <strong>用户交流与讨论：</strong>加入我们的<a
               href="[社区链接（如Discord、论坛等）]"
-              ><strong>[社区链接]</strong></a
+              ><strong>github社区</strong></a
             >，与其他用户、开发者共享使用经验，探讨技术问题。
           </li>
         </ul>
 
-        <h2>4. 版权与责任声明</h2>
-
         <p>
           尽管我们竭力保证软件的质量与适用性，但鉴于软件的开源性质与用户自行配置的灵活性，我们无法对因使用本软件而导致的任何直接、间接损失或特殊情况下引发的问题承担责任。用户在使用过程中应遵守相关法律法规，不得将本软件用于任何违法、侵权或损害公共利益的目的。
-        </p>
-
-        <p>
-          我们珍视每一位用户的信任与支持，将持续投入资源优化“<strong>[软件名]</strong>”，推动其成为VitePress社区不可或缺的工具之一。在此，我们诚邀您共同探索、共创，共筑高效、友好的VitePress配置生态。
         </p>
       </div>
     </div>

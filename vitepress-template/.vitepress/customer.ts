8d7b89ffb98be3cc6e54b45640105f5a6fec352f
ddd <1778871523@qq.com>
2024-06-11 16:55:39 +0800

    配置文件分离+引入大图插件

--- vitepress-template/.vitepress/customer.ts
+++ vitepress-template/.vitepress/customer.ts
@@ -1,4 +1,27 @@
+import mdItCustomAttrs from "markdown-it-custom-attrs";
+
 export const CustomerConfig = {
-  // head: [],
-  // markdown: {}
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "/bigimg/fancybox.css"//大图css
+      }
+    ],
+    [
+      "script",
+      {
+        src: "/bigimg/fancybox.umd.js"//大图js
+      }
+    ]
+  ],
+  markdown: {
+    config: (md) => {
+      // use more markdown-it plugins!
+      md.use(mdItCustomAttrs, "image", {
+        "data-fancybox": "gallery"
+      });
+    }
+  }
 };
